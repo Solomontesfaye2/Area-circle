6d34a4eba39774d8787f81b82e654103f00df70b
Solomon Tesfaye <solomon.tesfaye@ocsbstudent.ca>
2021-03-26 01:34:13 +0000

    body

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let diameter = 0
 let radius = 0
+let pie = 3.14
 let answer = 0
 
 document.getElementById('enter').addEventListener('click', enter)
-document.getElementById('submit').addEventListener('click, submit')
+document.getElementById('submit').addEventListener('click', submit)
 
 function enter () {
   diameter = document.getElementById('input').value
@@ -12,8 +13,8 @@ function enter () {
   alert(answer)
 }
 function submit () {
-  radius = document,getElementById('user-input').value
-   radius = parseInt(user-input)
-   answer = Math.PI; Math.pow(2, 2)
-   alert(answer)
+  radius = document.getElementById('input').value
+  radius = parseInt(input)
+  answer =  Math.PI;  radius
+  alert(answer)
 }
