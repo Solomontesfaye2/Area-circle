8924a6c734e89383cbe45cc9a4911b845134e72a
Solomon Tesfaye <solomon.tesfaye@ocsbstudent.ca>
2021-03-26 00:58:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let diameter = 0
+let radius = 0
+let answer = 0
+
+document.getElementById('enter').addEventListener('click', enter)
+document.getElementById('submit').addEventListener('click, submit')
+
+function enter () {
+  diameter = document.getElementById('input').value
+  diameter = parseInt(input)
+  answer = Math.PI;  diameter
+  alert(answer)
+}
+function submit () {
+  radius = document,getElementById('user-input').value
+   radius = parseInt(user-input)
+   answer = Math.PI; Math.pow(2, 2)
+   alert(answer)
+}
